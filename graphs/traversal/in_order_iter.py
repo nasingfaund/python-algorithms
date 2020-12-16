b60cde647bface59d2ad36f3b68081d337458611
funnydman <crazydzmitry@gmail.com>
2020-12-16 18:24:31 +0300

    Create in_order_iter.py

---
+++ graphs/traversal/in_order_iter.py
@@ -0,0 +1,11 @@
+def in_order_iter(root):
+    res, stack = [], []
+    curr = root
+    while curr or stack:
+        while curr:
+            stack.append(curr)
+            curr = curr.left
+        curr = stack.pop()
+        res.append(curr.val)
+        curr = curr.right
+    return res
