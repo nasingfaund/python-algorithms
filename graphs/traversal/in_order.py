1efec435c448d42e5f63f0c641d6dba7a0caef73
funnydman <crazydzmitry@gmail.com>
2020-12-16 18:24:11 +0300

    Update in_order.py

--- graphs/traversal/in_order.py
+++ graphs/traversal/in_order.py
@@ -1,19 +1,5 @@
-def in_order_iter(root):
-    res, stack = [], []
-    curr = root
-    while curr or stack:
-        while curr:
-            stack.append(curr)
-            curr = curr.left
-        curr = stack.pop()
-        res.append(curr.val)
-        curr = curr.right
-    return res
-
-
 def in_order(root):
     if root is not None:
         in_order(root.left)
         print(root.val)
         in_order(root.right)
-
