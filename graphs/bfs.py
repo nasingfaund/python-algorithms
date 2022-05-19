b8c31f7c68479c0c8918a66d4460723b28cde090
funnydman <crazydzmitry@gmail.com>
2022-05-19 15:05:52 +0200

    Update bfs.py

--- graphs/bfs.py
+++ graphs/bfs.py
@@ -12,15 +12,3 @@ def bfs(graph, source):
                 queue.append(n)
                 visited.add(n)
     return visited
-
-def bfs(graph, start):
-    queue = [start]
-    visited = [start]
-    while queue:
-        curr = queue.pop(0)
-        for v in graph[curr]:
-            if v not in visited:
-                queue.append(v)
-                visited.append(v)
-
-    return visited
