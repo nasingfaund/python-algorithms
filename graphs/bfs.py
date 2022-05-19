f27bccdc4373eb5687f40719200c3fec130ecce9
funnydman <crazydzmitry@gmail.com>
2022-05-19 15:01:30 +0200

    Update bfs.py

--- graphs/bfs.py
+++ graphs/bfs.py
@@ -1,3 +1,18 @@
+from collections import deque
+
+def bfs(graph, source):
+    queue = deque([source])
+    visited = {source}
+
+    while queue:
+        node = queue.popleft()
+
+        for n in graph[node]:
+            if n not in visited:
+                queue.append(n)
+                visited.add(n)
+    return visited
+
 def bfs(graph, start):
     queue = [start]
     visited = [start]
