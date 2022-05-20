ca4b0b1e742c44b9acfc77c6925d11e9d6e4b083
funnydman <crazydzmitry@gmail.com>
2022-05-20 16:19:50 +0200

    Update bfs.py

--- graphs/bfs.py
+++ graphs/bfs.py
@@ -1,5 +1,10 @@
 from collections import deque
 
+# bfs differs from dfs in two ways:
+# 1) it uses a queue (First In First Out) instead of a stack and
+# 2) it checks whether a vertex has been explored before enqueueing the vertex 
+# rather than delaying this check until the vertex is dequeued from the queue.
+
 def bfs(graph, source):
     queue = deque([source])
     visited = {source}
